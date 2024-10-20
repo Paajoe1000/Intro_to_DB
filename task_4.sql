7bd66637e738304d220ca6011704e2a57a7505c0
paajoe100 <kojoyankson12@gmail.com>
2024-10-20 22:04:38 +0100

    Add task 4

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+SHOW COLUMNS FROM BOOKS FROM alx_book_store;
+USE alx_book_store;
+
+SELECT(
+    TABLE_NAME,
+    COLUMN_NAME,
+    COLUMN_TYPE
+);
+FROM(
+    INFORMATION_SCHEMA.COLUMNS
+);
+WHERE(
+    TABLE_SCHEMA = "alx_book_store"
+    AND TABLE_NAME = "Books"
+);
